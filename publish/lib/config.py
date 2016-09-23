7d419a45ebd397513fea359fbec2657054d851ae
ace <343656921@qq.com>
2016-09-23 17:54:54 +0800

    fix

--- publish/lib/config.py
+++ publish/lib/config.py
@@ -1,6 +1,7 @@
 # encoding:utf-8
 
 import ConfigParser
+import os
 
 config = ConfigParser.ConfigParser()
-config.read('../config.ini')
+config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'config.ini'))
